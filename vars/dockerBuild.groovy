b510101898cecf589ade02955c160ba52333693a
adhig93 <81528871+adhig93@users.noreply.github.com>
2021-04-23 03:35:34 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -46,8 +46,7 @@ pipeline {
 					
 					stage('DEPLOY IMAGE') {
 						steps {
-						 sh '''
-ssh -i aws.pem ubuntu@172.31.38.250 ls '''
+						 sh ''ssh -i aws.pem ubuntu@172.31.38.250 ls''
 						}
 					}
 			}
