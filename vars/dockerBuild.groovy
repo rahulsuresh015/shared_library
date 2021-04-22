61e802f7c230c6ea0de239e1ebc82631bc5201fd
adhig93 <81528871+adhig93@users.noreply.github.com>
2021-04-23 01:26:20 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -29,7 +29,7 @@ pipeline {
 							 sh 'pwd'	
 							 sh 'ls -lrt'	
 							 script { 
-								 dockerImage = docker.build registry + ":" + dockerTag 
+								 dockerImage = docker.build $registry + ":" + $dockerTag 
 							 }
 						} 
 					}
@@ -37,7 +37,7 @@ pipeline {
 					stage('PUSH HUB') { 
 						 steps { 
 							 script { 
-								 docker.withRegistry( '', registryCredential ) { 
+								 docker.withRegistry( '', $registryCredential ) { 
 									 dockerImage.push() 
 									}
 							}		
