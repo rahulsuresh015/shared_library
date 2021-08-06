5863a0dc07798ea71ccfe1b3fc3d1cb5e694f0e8
adhig93 <81528871+adhig93@users.noreply.github.com>
2021-08-07 02:39:40 +0530

    Create dockerBuild3.groovy

---
+++ vars/dockerBuild3.groovy
@@ -0,0 +1,58 @@
+def call(String registryCred = 'a', String registryin = 'a', String docTag = 'a', String contName = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a') {
+
+pipeline {
+environment { 
+		registryCredential = "${registryCred}"
+		registry = "$registryin" 	
+		dockerTag = "${docTag}_$BUILD_NUMBER"
+		containerName = "${contName}"
+		gitRepo = "${grepo}"
+		gitBranch = "${gbranch}"
+		gitCredId = "${gitcred}"
+	}
+		
+	agent docker
+	
+	triggers {
+		pollSCM '* * * * *'
+	}
+
+	stages {
+		stage("POLL SCM"){
+			steps {
+				 git branch: "$gitRepo", credentialsId: "$gitCredId" , url: "$gitBranch"
+			}
+		}	
+					
+		stage('BUILD IMAGE') { 
+			 steps { 
+				 script { 
+					 dockerImage = docker.build registry + ":" + dockerTag 
+				 }
+			} 
+		}
+					
+		stage('PUSH HUB') { 
+			 steps { 
+				 script { 
+					 docker.withRegistry( '', registryCredential ) { 
+						 dockerImage.push() 
+					}
+				}		
+			} 
+		}
+					
+		stage('DEPLOY IMAGE') {
+			steps {
+				script { 
+					 docker.withRegistry( '', registryCredential ) { 
+						 dockerImage.run(-itd --name jenkinscontainer) 
+					}
+				} 
+			}
+		}
+	}
+			  
+}
+
+}
